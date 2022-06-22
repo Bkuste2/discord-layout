68c7e1aac38e623bd0b618699308b4caa8c40a4b
Bkuste2 <igorribeiro632@gmail.com>
2022-06-22 01:01:00 -0300

    New Commit

--- src/components/ChannelData/index.tsx
+++ src/components/ChannelData/index.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import ChannelMessage, { Mention } from "../ChannelMessage";
 import { Container, Messages, Input, InputWrapper, InputIcon } from "./styles";
 
 const ChannelData: React.FC = () => {
@@ -6,6 +7,99 @@ const ChannelData: React.FC = () => {
         <Container>
             
             <Messages>
+                <ChannelMessage
+                    author="Bkuste2"
+                    date="22/06/2022"
+                    content="To virando um monstro no react!"
+                />
+                <ChannelMessage
+                    author="Bkuste2"
+                    date="22/06/2022"
+                    content="To virando um monstro no react!"
+                />
+                <ChannelMessage
+                    author="Bkuste2"
+                    date="22/06/2022"
+                    content="To virando um monstro no react!"
+                />
+                <ChannelMessage
+                    author="Bkuste2"
+                    date="22/06/2022"
+                    content="To virando um monstro no react!"
+                />
+                <ChannelMessage
+                    author="Bkuste2"
+                    date="22/06/2022"
+                    content="To virando um monstro no react!"
+                />
+                <ChannelMessage
+                    author="Bkuste2"
+                    date="22/06/2022"
+                    content="To virando um monstro no react!"
+                />
+                <ChannelMessage
+                    author="Bkuste2"
+                    date="22/06/2022"
+                    content="To virando um monstro no react!"
+                />
+                <ChannelMessage
+                    author="Bkuste2"
+                    date="22/06/2022"
+                    content="To virando um monstro no react!"
+                />
+                <ChannelMessage
+                    author="Bkuste2"
+                    date="22/06/2022"
+                    content="To virando um monstro no react!"
+                />
+                <ChannelMessage
+                    author="Bkuste2"
+                    date="22/06/2022"
+                    content="To virando um monstro no react!"
+                />
+                <ChannelMessage
+                    author="Bkuste2"
+                    date="22/06/2022"
+                    content="To virando um monstro no react!"
+                />
+                <ChannelMessage
+                    author="Bkuste2"
+                    date="22/06/2022"
+                    content="To virando um monstro no react!"
+                />
+                <ChannelMessage
+                    author="Bkuste2"
+                    date="22/06/2022"
+                    content="To virando um monstro no react!"
+                />
+                <ChannelMessage
+                    author="Bkuste2"
+                    date="22/06/2022"
+                    content="To virando um monstro no react!"
+                />
+                <ChannelMessage
+                    author="Bkuste2"
+                    date="22/06/2022"
+                    content="To virando um monstro no react!"
+                />
+                <ChannelMessage
+                    author="Bkuste2"
+                    date="22/06/2022"
+                    content="To virando um monstro no react!"
+                />
+
+                <ChannelMessage
+                    author="Diego Fernandes"
+                    date="21/06/2020"
+                    content={
+                        <>
+                        <Mention>@Guilherme Rodz</Mention>, me carrega no LoL e CS de novo
+                        por favor?
+                        </>
+                    }
+                    hasMention
+                    isBot
+                />
 
             </Messages>
             <InputWrapper>
